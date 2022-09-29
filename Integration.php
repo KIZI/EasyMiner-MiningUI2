19db3a90b1762c12db8b00f28818ae66bb422726
Stanislav Vojíř <stanislav.vojir@gmail.com>
2022-09-29 14:31:55 +0200

    základ pro intergraci do EasyMinerCenter

---
+++ Integration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace EasyMiner\MiningUI2;
+
+/**
+ * Class Integration
+ * @package EasyMiner\MiningUI2
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ */
+class Integration {
+  /**
+   * @var $javascriptFiles - this array contains list of javascript files for inclusion into page
+   */
+  public static $javascriptFiles = [
+    //'js/main.js',    
+  ];
+
+  /**
+   * @var $cssFiles - this array contains list of CSS files for inclusion into page
+   */
+  public static $cssFiles = [
+    //'css/main.css'
+  ];
+
+}
